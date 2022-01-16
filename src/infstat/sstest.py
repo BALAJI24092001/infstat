3c9310160dda0fbbf70e157da2f3bc37caf3e0ab
BALAJI24092001 <dbalajivaraprasad@gmail.com>
2022-01-16 23:54:21 +0530

    one samp mean

--- src/infstat/sstest.py
+++ src/infstat/sstest.py
@@ -1,14 +1,24 @@
-class test:
-    def __init__(self, a, b):
-        self.a = a;
-        self.b = b;
-    def add(self):
-        print("Addition of the supplied numbers is: ", self.a+self.b);
-    def sub(self):
-        print("Subtraction of the supplied two numbers is :", self.a-self.b);
-    def mul(self):
-        print("Multiplication of the supplied two numbers is :", self.a*self.b);
-    def div(self):
-        print("Division of the supplied two numbers is :", self.a/self.b);
-temp = test(1, 2);
-temp.add();
+import pandas as pd
+import numpy as np
+class oneMean:
+    '''
+
+    '''
+    def __init__(self, data, mean, x=None):
+        dt = None;
+        df = int(0);
+        mean = int(0);
+        std = float(0);
+        p_val = float(0);
+        decision = str();
+        if type(data) == "<class 'pandas.core.frame.DataFrame'>":
+            if type(x) == "<class 'NoneType'>":
+                print(" "*10, "-"*10)
+                print("The column name 'x' is necessary for datatype DataFrame.")
+                print(" "*10, "-"*10)
+            else:
+                self.dt = np.array(data[x]);
+                self.mean = self.dt.mean();
+                self.std = self.dt.std();
+                self.df = np.prod(self.dt.shape);
+
