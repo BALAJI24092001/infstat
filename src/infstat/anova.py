9051e73a669d98f58da6beb5e671c7a79519458b
BALAJI <dbalajivaraprasad@gmail.com>
2022-04-01 23:55:05 +0530

    run test cri val database anova start

---
+++ src/infstat/anova.py
@@ -0,0 +1,3 @@
+class anova:
+    def __init__(self, data, alpha = 0.005, kind="CRD"):
+        pass
--- src/infstat/nonpar.py
+++ src/infstat/nonpar.py
@@ -7,12 +7,14 @@ class runTestAttributeError(Exception):
             super().__init__(message1)
         if leng == 'less':
             super().__init__(message2)
+class significanceLevelError(Exception):
+    def __init__(self, al):
+        super().__init__("The given alpha level database is yet to be updated, use alpha 0.005")
 
 class runTest():
     '''
-
     '''
-    def __init__(self, string, alpha=0.005):
+    def __init__(self, string):
         self.seq = list(string)
         self.count = len(set(string))
         self.uniqVals = list(set(self.seq))
@@ -31,7 +33,12 @@ class runTest():
                 self.count1 = self.count1 + 1
             elif self.seq[i] == self.uniqVals[1]:
                 self.count2 = self.count2 + 1
-    def solution(self, full = True):
+    def solution(self, full = True, alpha=0.005):
+        if alpha == 0.005:
+            lowerLimit = runTestCriValues[self.count1-2][self.count2][0];
+            upperLimit = runTestCriValues[self.count1-2][self.count2][1]
+        else:
+            raise significanceLevelError(al = alpha)
         hypothesis = """
         RUN TEST FOR RANDOMNESS
         ````````````````````````
@@ -42,21 +49,21 @@ class runTest():
 
         """
         step1 = "Step 1: Determine the run length 'R'."
-        step2 = "we got the total number of runs = " + str(self.runs) + "."
-        step3 = "we got the count of first attritbute " + str(self.uniqVals[0]) + "(m) = " + str(self.count1)
-        step4 = "we got the count of first attritbute " + str(self.uniqVals[1]) + "(n) = " + str(self.count2)
-        step5 = "Considering the alpha level of 5%(0.005), the critical region for the run test is given below: "
-        step6 = "Check if the numbers of runs in the sequence, with m and n attributes of same time lies in the interval form the run test table."
-        step7 = "The lower limit is " + str( runTestCriValues[self.count1-2][self.count2][0] ) + " and upper limit is " + str(runTestCriValues[self.count1-2][self.count2][1])
-        step8 = "Comparing the run length with the critical values, we get"
-        if(runTestCriValues[self.count1-2][self.count2][0] <= self.runs):
-            if(self.runs == None or runTestCriValues[self.count1-2][self.count2][1] >= self.runs):
-                step9 = "Since the run length lies in the interval, we fail to reject the null hypothesis."
+        step2 = "Step 2: we got the total number of runs = " + str(self.runs) + "."
+        step3 = "Step 3: we got the count of first attritbute " + str(self.uniqVals[0]) + "(m) = " + str(self.count1)
+        step4 = "Step 4: we got the count of first attritbute " + str(self.uniqVals[1]) + "(n) = " + str(self.count2)
+        step5 = "Step 5: Considering the alpha level of 5%(0.005), the critical region for the run test is given below: "
+        step6 = "Step 6: The lower limit is " + str( lowerLimit ) + " and upper limit is " + str( upperLimit )
+        step7 = "Step 7: Comparing the run length with the critical values, we get"
+        if(lowerLimit <= self.runs):
+            if(self.runs == None or upperLimit >= self.runs):
+                step8 = "Step 9: Since the run length lies in the interval, we fail to reject the null hypothesis."
             else:
-                step9 = "Since the run length does not lie in the interval, we reject the null hypothesis."
+                step8 = "Step 9: Since the run length does not lie in the interval, we reject the null hypothesis."
         else:
-            step9 = "Since the run length does not lie in the interval, we reject the null hypothesis."
-
+            step8 = "Step 9: Since the run length does not lie in the interval, we reject the null hypothesis."
+        
+        step9 = "m = " + str(self.count1) + ", n = " + str(self.count2) + "\n" + "Limits: " + str(lowerLimit) + ", " + str(upperLimit) + "\n" + "Run length : " + str(self.runs)
         steps = [step1, step2, step3, step4, step5, step6, step7, step8, step9]
         for i in steps:
             print(i)
